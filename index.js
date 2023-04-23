b6fecfde27f7d403e541a53bf7f20eaf4a898cf2
fgm0129 <563508194@qq.com>
2023-04-24 00:25:58 +0800

    fix:add require openai

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const bodyParser = require("koa-bodyparser");
 const fs = require("fs");
 const path = require("path");
 const { init: initDB, Counter } = require("./db");
+const { Configuration, OpenAIApi } = require("openai");
+
 
 const router = new Router();
 
